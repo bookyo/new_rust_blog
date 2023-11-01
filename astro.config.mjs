de7d5ed4079cd998b4452e8113d3e94608ebce54
unknown <mwm0022@126.com>
2023-11-01 22:55:28 +0800

    add new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import denoAdapter from '@astrojs/deno';
 import react from "@astrojs/react";
-import nodejs from '@astrojs/node';
+import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: nodejs(),
-  integrations: [react(), tailwind()],
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [react(), tailwind()]
 });
