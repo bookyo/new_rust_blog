216c2371b3a2048d1804d3e52af0a079e9627c23
unknown <mwm0022@126.com>
2023-11-01 22:27:53 +0800

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import denoAdapter from '@astrojs/deno';
 import react from "@astrojs/react";
-
+import nodejs from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: denoAdapter(),
+  adapter: nodejs(),
   integrations: [react(), tailwind()],
 });
